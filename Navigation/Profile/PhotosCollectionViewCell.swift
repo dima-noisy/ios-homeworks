e4e62d2bab23740fea90cb911b47e89abcc501bb
Dmitry Poryadin <dporyadin11@gmail.com>
2024-09-20 00:39:32 +0300

    in process commit

--- Navigation/Profile/PhotosCollectionViewCell.swift
+++ Navigation/Profile/PhotosCollectionViewCell.swift
@@ -35,7 +35,52 @@ class PhotosCollectionViewCell: UICollectionViewCell {
         ])
     }
     
+    func setup(with photo: String) {
+        photoView.image = UIImage(named: photo)
+    }
+}
+
+
+/*
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+
+    private lazy var photoView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        contentView.addSubview(photoView)
+        setupView()
+        setupLayouts()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        contentView.clipsToBounds = true
+    }
+    
+    private func setupLayouts() {
+        NSLayoutConstraint.activate([
+            photoView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            photoView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            photoView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            photoView.heightAnchor.constraint(equalTo: contentView.heightAnchor)
+        ])
+    }
+    
     func setup(with photo: UIImage) {
         photoView.image = photo
     }
 }
+*/
